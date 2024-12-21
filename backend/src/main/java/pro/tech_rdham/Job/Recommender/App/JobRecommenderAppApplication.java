dad50b29e249ed9968d170c28dffa4598a4217bb
Rohit Dham <rdhammpls@gmail.com>
2024-12-21 16:45:29 -0600

    The beginning

---
+++ backend/src/main/java/pro/tech_rdham/Job/Recommender/App/JobRecommenderAppApplication.java
@@ -0,0 +1,13 @@
+package pro.tech_rdham.Job.Recommender.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecommenderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRecommenderAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pro/tech_rdham/Job/Recommender/App/JobRecommenderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.tech_rdham.Job.Recommender.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecommenderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
