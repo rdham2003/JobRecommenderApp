da2e1a46f3b95a87b263348b4dfbb688fba1d1b5
Rohit Dham <rdhammpls@gmail.com>
2025-01-10 20:41:52 -0600

    Fixed minor bug in Service file

--- backend/src/main/java/pro/tech_rdham/Job/Recommender/App/Service.java
+++ backend/src/main/java/pro/tech_rdham/Job/Recommender/App/Service.java
@@ -52,7 +52,7 @@ public class Service {
             apiUrl.append(API_ID);
             apiUrl.append("&app_key=");
             apiUrl.append(API_KEY);
-            apiUrl.append("&results_per_page=50&what_or=intern%2C%20");
+            apiUrl.append("&results_per_page=50&what_or=intern%2C%20internship%2C%20coop%2C%20");
             apiUrl.append(skillset);
             apiUrl.append("&where=") ;
             apiUrl.append(location) ;
